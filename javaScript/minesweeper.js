f3340df843403c7cec91eb8058bf606d453bc680
rogeliost <rosuarez@birt.eus>
2025-03-20 15:51:42 +0100

    corregida posicion inicial y coloreada verde

--- javaScript/minesweeper.js
+++ javaScript/minesweeper.js
@@ -9,7 +9,6 @@ const CELL_CLOSED = 0,
 
 class Minesweeper extends Game {
     #boardState; // array de estado de las casillas del tablero 
-
     constructor(name, fetchManager) {
         super(name);
         this.initGame(fetchManager, fetchManager.gameTypes.Minesweeper);
@@ -17,21 +16,20 @@ class Minesweeper extends Game {
 
     generateGame() {
         console.log("--Creando nuevo juego");
-        //const this.data = await this.#callNewGameAPI();//llamada a la API por un nuevo juego  
-        let initCoords = this.data["start"];
+        let initCoords = this.data.start;
         let index = initCoords.indexOf("-");
-        this.startx = parseInt(initCoords.substring(0, index)); // posicion inicial desde 1 a width
-        this.starty = parseInt(initCoords.substring(index + 1)); // posicion inicial desde 1 a height
-        this.width = this.data["width"] - 1;
-        this.height = this.data["height"] - 1;
-        this.mines = this.data["mines"];
-        this.board = this.data["board"];
-        // console.log(this.startx+" "+this.starty+" "+this.width+" "+this.height+" "+this.board);
+        this.startx = parseInt(initCoords.substring(0, index))-1; // posicion inicial desde 1 a width, restamos 1 para homogeneizar
+        this.starty = parseInt(initCoords.substring(index + 1))-1; // posicion inicial desde 1 a height, restamos 1 para homogeneizar
+        this.width = this.data.width;
+        this.height = this.data.height;
+        this.mines = this.data.mines;
+        this.board = this.data.board;
         this.#initMinesweeperBoard();
-        this.drawBoardFromApi();
+        this.drawBoard();
     }
 
-    async #initMinesweeperBoard() { // Inicia el estado del tablero a todos cerrados
+    // Inicia el estado del tablero a todos cerrados
+    async #initMinesweeperBoard() { 
         console.log("--Inicializando estado del tablero");
         console.log("tamaño: " + this.width + "/" + this.height);
         this.#boardState = new Array(this.height);
@@ -53,7 +51,8 @@ class Minesweeper extends Game {
         this.#boardState[y][x] = state;
     }
 
-    openCell(y, x) { // abrir y procesar celdas alrededor de x,y, devolver true si hay bomba, false si no
+    // abrir y procesar celdas alrededor de x,y, devolver true si hay bomba, false si no
+    openCell(y, x) { 
         let bomb = false;
         if (this.getStateCell(y, x) == CELL_CLOSED) {
             this.setStateCell(y, x, CELL_OPENED);
@@ -73,10 +72,12 @@ class Minesweeper extends Game {
         return bomb;
     }
 
-    markCell(y, x) {
-        /*
-        si la celda esta cerrada
-            cambiar estado de la celda a marked */
+    switchCellMark(y, x) {
+        if (this.getStateCell(y, x)==CELL_MARKED) {
+            this.setStateCell(CELL_CLOSED)
+        } else {
+            this.setStateCell(CELL_MARKED);
+        }
     }
 
     /*async #callNewGameAPI() {
@@ -100,33 +101,49 @@ class Minesweeper extends Game {
 }*/
 
     drawCell(y, x) { //de momento no dibuja nada, solo devuelve una string para consola
-        let content = this.getCellContent(y, x);//board[y][x];
-        let state = this.getStateCell(y, x);
-        let temp = ""; // TODO temporalmente modo texto
-        //posicionarse en y,x
-        switch (state) {
-            case CELL_CLOSED: temp = " . "; //dibujar la celda cerrada
-                break;
-            case CELL_MARKED: temp = " F "; //dibujar la bandera  
-                break;
-            case CELL_OPENED: temp = " " + content + " "; //dibujar el numero         
+        this.context.font = "25px Arial";
+        let color=this.boxColor;
+        if (y==this.starty && x==this.startx) {
+            color="green";
+            console.log("posicion inicial green");
         }
-        return temp;
+        this.context.fillStyle = color;
+
+        // dibuja el fondo de la celda
+        this.context.fillRect((
+            this.boxSize + this.offset) * x + this.offset, 
+            (this.boxSize + this.offset) * y + this.offset, this.boxSize, this.boxSize);
+
+        // dibuja el contenido de la celda
+        this.context.fillStyle = this.textColor;
+        let content="";
+        let state = this.getStateCell(y, x);
+        switch (state) {
+            case CELL_CLOSED: 
+                content="";
+                break;
+            case CELL_MARKED: 
+                content="F";
+                break;
+            case CELL_OPENED: 
+                content = this.getCellContent(y, x);
+                break;
+        }
+        this.context.fillText(
+            content, 
+            (this.boxSize + this.offset) * x + (this.boxSize / 2), 
+            (this.boxSize + this.offset) * y + (this.boxSize));
     }
 
     drawBoard() {
-        console.log("x: " + this.startx + " y:" + this.starty);
         for (let y = 0; y < this.height; y++) {
-            let temp = "";
             for (let x = 0; x < this.width; x++) {
-                //this.drawCell(x,y); TODO Se hara cuando se pueda, de momento modo texto
-                temp += this.drawCell(y, x);
+                this.drawCell(y, x); 
             }
-            console.log(temp); //de momento lo mostramos en modo texto
         }
     }
 
-    drawBoardFromApi() {
+    /*drawBoardFromApi() {
         this.context.font = "25px Arial";
         for (let width = 0; width < 9; width++) {
             for (let height = 0; height < 9; height++) {
@@ -145,7 +162,7 @@ class Minesweeper extends Game {
                 }
             }
         }
-    }
+    }*/
 
 }
 let minesweeper = new Minesweeper("minesweeperCanvas",fetchManager);
