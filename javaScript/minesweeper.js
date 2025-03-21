a4f744908064bf4cdd043045929980fe702e517a
rogeliost3 <rogeliost2008@gmail.com>
2025-03-21 10:42:37 +0100

    empleo de metodos de clase Game

--- javaScript/minesweeper.js
+++ javaScript/minesweeper.js
@@ -17,51 +17,63 @@ class Minesweeper extends Game {
         this.initGame(fetchManager, fetchManager.gameTypes.Minesweeper);
     }
 
-    generateGame() {
-        let initCoords = this.data.start;
+     init(){
         let index = initCoords.indexOf("-");
         this.startx = parseInt(initCoords.substring(0, index))-1; // posicion inicial desde 1 a width, restamos 1 para homogeneizar
-        this.starty = parseInt(initCoords.substring(index + 1))-1; // posicion inicial desde 1 a height, restamos 1 para homogeneizar
-        this.width = this.data.width;
-        this.height = this.data.height;
+        this.starty = parseInt(initCoords.substring(index + 1))-1; 
         this.mines = this.data.mines;
-        this.board = this.data.board;
-        this.#initMinesweeperBoard();
-        this.drawBoard();
+        // this.#initMinesweeperBoard();
     }
 
-    // Inicia el estado del tablero a todos cerrados
-    async #initMinesweeperBoard() { 
-        this.#boardState = new Array(this.height);
-        for (let y = 0; y < this.height; y++) {
-            this.#boardState[y] = new Array(this.width).fill(CELL_CLOSED);
+    generateGame() {
+        let cellList = [];
+        for (let width = 0; width < this.size; width++) {
+            for (let height = 0; height < this.size; height++) {
+                cellList.push(new Cell(
+                    this.canvas, this.context,
+                    this.boxSize, this.offset,
+                    this.data.board[width][height], //TODO fila / columna ??
+                    width, height,
+                    this.boxColor, this.textColor
+                ));
+            }
         }
-        this.openCell(this.starty, this.startx); //abrir la celda inicial
+        return cellList;
     }
 
-    getCellContent(y, x) {
-        return this.board[y][x];
-    }
+    
+    // Inicia el estado del tablero a todos cerrados
+    // async #initMinesweeperBoard() { 
+    //     this.#boardState = new Array(this.size);
+    //     for (let y = 0; y < this.height; y++) {
+    //         this.#boardState[y] = new Array(this.width).fill(CELL_CLOSED);
+    //     }
+    //     this.openCell(this.starty, this.startx); //abrir la celda inicial
+    // }
 
-    getCellState(y, x) {
-        return this.#boardState[y][x];
-    }
+    // getCellText(y, x) {
+    //     return this.board[y][x];
+    // }
 
-    setCellState(y, x, state) {
-        this.#boardState[y][x] = state;
-    }
+    // getCellState(y, x) {
+    //     return this.#boardState[y][x];
+    // }
+
+    // setCellState(y, x, state) {
+    //     this.#boardState[y][x] = state;
+    // }
 
     // abrir y procesar celdas alrededor de x,y, devolver true si hay bomba, false si no
-    openCell(y, x) { 
+    openCell(x, y) { 
         let bomb = false;
-        if (this.getCellState(y, x) == CELL_CLOSED) {
+        if (this.getCellState(x, y) == CELL_CLOSED) {
             // this.setCellState(y, x, CELL_OPENED);
             for (let posy = y - 1; posy <= y + 1; posy++) {
                 for (let posx = x - 1; posx <= x + 1; posx++) {
                     if (posx > -1 && posy > -1 && posx < this.width && posy < this.height) {
                         if (this.getCellState(posy, posx) == CELL_CLOSED) {
                             this.setCellState(posy, posx, CELL_OPENED);
-                            if (this.getCellContent(y, x) == "x") {
+                            if (this.getCellText(y, x) == "x") {
                                 bomb = true;
                             }
                         }
@@ -79,7 +91,7 @@ class Minesweeper extends Game {
             }
         }
     }
-    switchCellMark(y, x) {
+    switchCellMark(x, y) {
         if (this.getCellState(y, x)==CELL_MARKED) {
             this.setCellState(CELL_CLOSED)
         } else {
@@ -87,48 +99,48 @@ class Minesweeper extends Game {
         }
     }
 
-    drawCell(y, x) {
-        this.context.font = "25px Arial";
-        let color=this.boxColor;
-        if (y==this.starty && x==this.startx) {
-            color="green";
-            console.log("posicion inicial green");
-        }
-        this.context.fillStyle = color;
+    // drawCell(y, x) {
+    //     this.context.font = "25px Arial";
+    //     let color=this.boxColor;
+    //     if (y==this.starty && x==this.startx) {
+    //         color="green";
+    //         console.log("posicion inicial green");
+    //     }
+    //     this.context.fillStyle = color;
 
-        // dibuja el fondo de la celda
-        this.context.fillRect((
-            this.boxSize + this.offset) * x + this.offset, 
-            (this.boxSize + this.offset) * y + this.offset, this.boxSize, this.boxSize);
+    //     // dibuja el fondo de la celda
+    //     this.context.fillRect((
+    //         this.boxSize + this.offset) * x + this.offset, 
+    //         (this.boxSize + this.offset) * y + this.offset, this.boxSize, this.boxSize);
 
-        // dibuja el contenido de la celda
-        this.context.fillStyle = this.textColor;
-        let content="";
-        let state = this.getStateCell(y, x);
-        switch (state) {
-            case CELL_CLOSED: 
-                content="";
-                break;
-            case CELL_MARKED: 
-                content="F";
-                break;
-            case CELL_OPENED: 
-                content = this.getCellContent(y, x);
-                break;
-        }
-        this.context.fillText(
-            content, 
-            (this.boxSize + this.offset) * x + (this.boxSize / 2), 
-            (this.boxSize + this.offset) * y + (this.boxSize));
-    }
+    //     // dibuja el contenido de la celda
+    //     this.context.fillStyle = this.textColor;
+    //     let content="";
+    //     let state = this.getCellState(y, x);
+    //     switch (state) {
+    //         case CELL_CLOSED: 
+    //             content="";
+    //             break;
+    //         case CELL_MARKED: 
+    //             content="F";
+    //             break;
+    //         case CELL_OPENED: 
+    //             content = this.getCellText(y, x);
+    //             break;
+    //     }
+    //     this.context.fillText(
+    //         content, 
+    //         (this.boxSize + this.offset) * x + (this.boxSize / 2), 
+    //         (this.boxSize + this.offset) * y + (this.boxSize));
+    // }
 
-    drawBoard() {
-        for (let y = 0; y < this.height; y++) {
-            for (let x = 0; x < this.width; x++) {
-                this.drawCell(y, x); 
-            }
-        }
-    }
+    // drawBoard() {
+    //     for (let y = 0; y < this.height; y++) {
+    //         for (let x = 0; x < this.width; x++) {
+    //             this.drawCell(y, x); 
+    //         }
+    //     }
+    // }
 
 }
 
